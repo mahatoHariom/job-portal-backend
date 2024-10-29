1940b476e9d7a0efdc0b2099f735c319deb6f55d
mahatohariom <mahatohariom31@gmail.com>
2024-10-29 06:02:28 +0545

    componnet rregistery refactor

--- src/app/middlewares/errorHandler.ts
+++ src/app/middlewares/errorHandler.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import ApiError from '@/infrastructure/config/ApiError'
 import { FastifyError, FastifyReply, FastifyRequest } from 'fastify'
+
 export const errorHandler = (error: FastifyError | ApiError | Error, request: FastifyRequest, reply: FastifyReply) => {
   if (error instanceof ApiError) {
     reply.status(error.statusCode).send({
@@ -8,17 +9,18 @@ export const errorHandler = (error: FastifyError | ApiError | Error, request: Fa
       error: error.statusMessage,
       message: error.message
     })
+  } else if ((error as any).code === 'P2002') {
+    reply.status(409).send({
+      statusCode: 409,
+      error: 'Conflict',
+      message: 'A record with the same unique field already exists, such as an email or username'
+    })
   } else {
-    reply.status(500).send(error)
+    // Handle any other unexpected errors
+    reply.status(500).send({
+      statusCode: 500,
+      error: 'Internal Server Error',
+      message: 'An unexpected error occurred'
+    })
   }
 }
-
-export const asyncErrorHandler =
-  <T extends { Body: any }>(handler: (request: FastifyRequest<T>, reply: FastifyReply) => Promise<void>) =>
-  async (request: FastifyRequest<T>, reply: FastifyReply) => {
-    try {
-      await handler(request, reply)
-    } catch (error: any) {
-      errorHandler(error, request, reply)
-    }
-  }
